96354e089b7fec009866bd96e7d66894a0abaf1b
Amitava ghosh <amitava@Amitavas-MacBook-Air.local>
2023-07-02 14:54:29 +0530

    updating different files

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const serverconfig =require('./configs/server.configs');
 
 const app = express();
 
 
-app.listen(3113, ()=>{
-    console.log("server started");
+app.listen(7777, ()=>{
+    console.log(`server started on the port number ${serverconfig.port}`);
 })
